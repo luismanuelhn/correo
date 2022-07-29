8726e8e5d8a40e0546ec5c61b8c8c0c8778b80ab
luismanuelhn <luismanuelhn@gmail.com>
2022-07-28 21:38:58 -0600

    creando index

---
+++ modal.php
@@ -0,0 +1,27 @@
+<div class="modal fade" id="modalsuscribete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog h-100 d-flex flex-column justify-content-center my-0" role="document">
+        <form method="post" id="usuario_form">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h3 class="modal-title">Suscríbete Ahora</h3>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">@</span>
+                        </div>
+                        <input type="email" id="usu_correo" name="usu_correo" class="form-control" placeholder="Correo electronico" aria-describedby="basic-addon1" required>
+                    </div>
+                    <p>Para registrarte en ofertas increíbles, gratis y otras cosas interesantes, quedese con nosotros y suscríbase.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" name="action" id="#" value="add" class="btn btn-primary">Enviar</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    
+</div>
